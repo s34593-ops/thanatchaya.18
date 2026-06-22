7ea237d7b5cfdc1232969aae545823cc0bbbdee6
s34593-ops <s34593@banbung.ac.th>
2026-06-22 11:42:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// เพิ่ม JavaScript สไหรับการโต้ตอบกับผู้ใช้   (Optional)
-document.addEventListener('DOMContentLoaded' ,function () {
-  console.log('Portfolio ของ THANATCHAYA พร้อมแล้ว!');
+// เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Portfolio ของ THANATCHAYA พร้อมแล้ว!');
 });
